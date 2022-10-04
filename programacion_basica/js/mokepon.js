d104614060294f4a46e6a737d64bda7c2c79ba2a
Daniel Cárdenas Rodríguez <daniel24cardenasr@gmail,com>
2022-10-03 19:42:54 -0500

    Se agrega la aleatoriedad de la mascota enemiga

--- programacion_basica/js/mokepon.js
+++ programacion_basica/js/mokepon.js
@@ -3,16 +3,37 @@ function iniciarJuego(){
 	botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 function seleccionarMascotaJugador(){
-	if(document.getElementById('hipodogue').checked){
-		alert('has seleciccionado a hipodogue')
-	}else if(document.getElementById('capipepo').checked){
-		alert('has seleciccionado a capipepo')
-	}else if(document.getElementById('ratigueya').checked){
-		alert('has seleciccionado a capipepo')
+	let inputHipodogue = document.getElementById('hipodogue')
+	let inputCapipepo = document.getElementById('capipepo')
+	let inputRatigueya = document.getElementById('ratigueya')
+	let spanMascotaJugador = document.getElementById('mascota-jugador')
+	
+	if(inputHipodogue.checked){
+		spanMascotaJugador.innerHTML = "Hipodogue"
+	}else if(inputCapipepo.checked){
+		spanMascotaJugador.innerHTML = "Capipepo"
+	}else if(inputRatigueya.checked){
+		spanMascotaJugador.innerHTML = "Ratigueya"
 	}else{
 		alert('Porfavor selecciona algún animal')
 	}
+	seleccionarMascotaEnemigo()
 }
 
+function seleccionarMascotaEnemigo(){
+	let spanMascotaEnemigo = document.getElementById('mascota-oponente')
+	let ataqueAleatorio = aleatorio(1,3)
+	
+	if (ataqueAleatorio == 1){
+		spanMascotaEnemigo.innerHTML = 'Hipodogue'
+	}else if(ataqueAleatorio == 2){
+		spanMascotaEnemigo.innerHTML = 'Capipepo'
+	}else{
+		spanMascotaEnemigo.innerHTML = 'Ratigueya'
+	}
+}
+function aleatorio(min, max){
+	return Math.floor(Math.random()*(max-min+1)+min)
+}
 
 window.addEventListener('load', iniciarJuego)
